84c7d516b430cbdb76d91396dd4180d080f85188
ZeNoHmin <164506667+ZeNoHmin@users.noreply.github.com>
2024-05-11 11:47:14 +0200

    Small change

---
+++ php/include/navimport.php
@@ -0,0 +1,17 @@
+<h1>Ink & Inspiration</h1>
+<div class="pos-f-t">
+<nav class="navbar navbar-dark bg-dark">
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+      </nav>
+  <div class="collapse" id="navbarToggleExternalContent">
+    <div class="bg-dark p-4">
+    <ul class="list-unstyled">
+      <li><a href="index.php">Index</a></li>
+      <li><a href="index.php">Index</a></li>
+      <li><a href="index.php">Index</a></li>
+    </ul>
+    </div> 
+  </div>
+</div>
