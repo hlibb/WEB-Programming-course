73a8cb568af91a4f23b025eb3c61e27fee235692
ZeNoHmin <164506667+ZeNoHmin@users.noreply.github.com>
2024-05-20 23:18:13 +0200

    Update database_init.sql
    Login timestamp screen resolution operating system und points awarded hinzugefügt

--- database/database_init.sql
+++ database/database_init.sql
@@ -11,6 +11,10 @@ CREATE TABLE IF NOT EXISTS `users` (
     `email` VARCHAR(255) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `address` VARCHAR(255)
+    `login_timestamp` DATETIME NOT NULL,
+    `screen_resolution` VARCHAR(20) NOT NULL,
+    `operating_system` VARCHAR(20) NOT NULL,
+    `points_awarded` INT NOT NULL DEFAULT 100,
 );
 
 -- Table for storing product information
