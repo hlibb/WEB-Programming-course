59147e65e247d714c4ae158dbf289861eab110eb
Hlib Astashev <Astashev01@gmail.com>
2024-06-06 14:18:32 +0200

    ^commit test

--- php/include/headimport.php
+++
@@ -1,9 +0,0 @@
-<!-- Bootstrap -->
-<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-<!-- Fontawesome -->
-<link href="../node_modules/@fortawesome/fontawesome-free/css/all.css" rel="stylesheet">
-<!-- CSS -->
-<link rel="stylesheet" href="styles.css">
-<?php
-  include "../fetch_online_users.php"
-?>
