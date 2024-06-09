d1194aba7f70010957e347e175ed5d8af24cfafd
ZeNoHmin <164506667+ZeNoHmin@users.noreply.github.com>
2024-06-09 11:58:44 +0200

    Update navimport.php

--- php/include/navimport.php
+++ php/include/navimport.php
@@ -4,17 +4,17 @@
     <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
-    <a href="warenkorb.php"><button type="button" class="btn btn-primary">
+    <a href="shopping_cart.php"><button type="button" class="btn btn-primary">
       Warenkorb <span class="badge text-bg-secondary">X</span>
     </button></a>
       </nav>
   <div class="collapse" id="navbarToggleExternalContent">
     <div class="bg-dark p-4">
     <ul class="list-unstyled">
-      <li><a href="../php/login/login.php">Login</a></li>
-      <li><a href="../php/registration/registration.php">Registrierung</a></li>
-      <li><a href="../index.html">Index</a></li>
-        <li><a href="../php/about.php">About Us</a></li>
+      <li><a href="login.php">Login</a></li>
+      <li><a href="registration.php">Registrierung</a></li>
+      <li><a href="home.php">Home</a></li>
+        <li><a href="about.php">About Us</a></li>
     </ul>
     </div> 
   </div>
