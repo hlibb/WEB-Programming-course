f4bda26383a0fe42f1b4d1ec8c5ce7518b09358f
ZeNoHmin <164506667+ZeNoHmin@users.noreply.github.com>
2024-06-09 19:24:30 +0200

    Update navimport.php

--- php/include/navimport.php
+++ php/include/navimport.php
@@ -1,18 +1,18 @@
-<h1>Ink & Inspiration</h1>
+<a href="home.php"><img src="../assets/images/logo.png" class="logo"></a>
 <div class="pos-f-t">
 <nav class="navbar navbar-dark bg-dark">
     <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <a href="shopping_cart.php"><button type="button" class="btn btn-primary">
-      Warenkorb <span class="badge text-bg-secondary">X</span>
+      Warenkorb <span class="badge text-bg-secondary">x</span>
     </button></a>
-      </nav>
+</nav>
   <div class="collapse" id="navbarToggleExternalContent">
     <div class="bg-dark p-4">
     <ul class="list-unstyled">
-      <li><a href="login.php">Login</a></li>
-      <li><a href="registration.php">Registrierung</a></li>
+      <li><a href="checkout.php">Checkout</a></li>
+      <li><a href="artikeluebersicht.php">Artikelübersicht</a></li>
       <li><a href="home.php">Home</a></li>
         <li><a href="about.php">About Us</a></li>
     </ul>
