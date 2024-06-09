909a372f40c3352692b684687a86de7c9dbd16ea
ZeNoHmin <164506667+ZeNoHmin@users.noreply.github.com>
2024-06-09 16:49:34 +0200

    Create send_email.php

---
+++ php/send_email.php
@@ -0,0 +1,40 @@
+<?php
+// send_email.php
+
+// Include PHPMailer classes
+require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
+require 'phpmailer/src/Exception.php';
+require 'email_templates.php'; // Include the email templates
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+function sendEmail($recipientEmail, $recipientName, $emailTemplate) {
+    $mail = new PHPMailer(true);
+    try {
+        // Server settings
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com'; // Set the SMTP server to send through
+        $mail->SMTPAuth = true;
+        $mail->Username = 'webprogrammierung27@gmail.com'; // SMTP username
+        $mail->Password = 'eiih ilpp viyx xdzu'; // SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;        
+
+        // Recipients
+        $mail->setFrom('webprogrammierung27@gmail.com', 'Ink & Inspiration');
+        $mail->addAddress($recipientEmail, $recipientName); // Add a recipient
+
+        // Content
+        $mail->isHTML(true); // Set email format to HTML
+        $mail->Subject = $emailTemplate['subject'];
+        $mail->Body    = $emailTemplate['body'];
+
+        $mail->send();
+        echo 'Message has been sent';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
