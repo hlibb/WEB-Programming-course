0f6329ba57839695b159b6069eee29973f12d3e3
ZeNoHmin <dimi13psomas@gmail.com>
2024-06-09 20:05:36 +0200

    Commit STYLE

--- php/include/navimport.php
+++ php/include/navimport.php
@@ -1,21 +1,21 @@
 <a href="home.php"><img src="../assets/images/logo.png" class="logo"></a>
 <div class="pos-f-t">
-<nav class="navbar navbar-dark bg-dark">
-    <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <a href="shopping_cart.php"><button type="button" class="btn btn-primary">
-      Warenkorb <span class="badge text-bg-secondary">x</span>
-    </button></a>
-</nav>
-  <div class="collapse" id="navbarToggleExternalContent">
-    <div class="bg-dark p-4">
-    <ul class="list-unstyled">
-      <li><a href="checkout.php">Checkout</a></li>
-      <li><a href="artikeluebersicht.php">Artikelübersicht</a></li>
-      <li><a href="home.php">Home</a></li>
-        <li><a href="about.php">About Us</a></li>
-    </ul>
-    </div> 
-  </div>
+    <nav class="navbar navbar-dark bg-dark">
+        <button class="navbar-toggler ml-2" type="button" data-toggle="collapse" data-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <a href="shopping_cart.php"><button type="button" class="btn btn-primary">
+                Warenkorb <span class="badge text-bg-secondary">x</span>
+            </button></a>
+    </nav>
+    <div class="collapse" id="navbarToggleExternalContent">
+        <div class="bg-dark p-4">
+            <ul class="list-unstyled">
+                <li><a href="checkout.php">Checkout</a></li>
+                <li><a href="artikeluebersicht.php">Artikelübersicht</a></li>
+                <li><a href="home.php">Home</a></li>
+                <li><a href="about.php">About Us</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
